c2908879887deced00f38aabfdf652ce216910fd
playniuniu <playniuniu@gmail.com>
2016-10-24 20:58:07 +0800

    modify dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,21 @@ FROM alpine:latest
 MAINTAINER playniuniu < playniuniu@gmail.com >
 
 ENV BUILD_DEP build-base python-dev py-virtualenv libffi-dev openssl-dev
-ENV PACKAGE_BASE python
+ENV PACKAGE_BASE python openssh sshpass
 
 WORKDIR /usr/src/app/
 
 RUN apk add --update --no-cache $PACKAGE_BASE $BUILD_DEP \
     && virtualenv ./env \
-    && /usr/src/app/env/bin/pip install cffi \
-    && /usr/src/app/env/bin/pip install ansible pywinrm \
+    && /usr/src/app/env/bin/pip install --no-cache-dir cffi \
+    && /usr/src/app/env/bin/pip install --no-cache-dir ansible pywinrm \
     && apk del $BUILD_DEP \
     && rm -rf /var/cache/apk/*
 
-CMD ["/bin/sh"]
+VOLUME /data
+COPY entrypoint.sh /usr/sbin
+
+EXPOSE 22
+
+ENTRYPOINT ["/usr/sbin/entrypoint.sh"]
+CMD ["/usr/sbin/sshd","-D"]
---
+++ entrypoint.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# change sshd_config
+if [ -f "/etc/ssh/sshd_config" ]; then
+    sed -ri 's/^PermitRootLogin\s+.*/PermitRootLogin yes/' /etc/ssh/sshd_config \
+    && sed -ri 's/^PasswordAuthentication\s+.*/PasswordAuthentication no/' /etc/ssh/sshd_config \
+    && sed -ri 's/^HostKey\s+.*//' /etc/ssh/sshd_config
+fi
+
+# prepare run dir
+if [ ! -d "/var/run/sshd" ]; then
+    mkdir -p /var/run/sshd
+fi
+
+# prepare root ssh folder
+if [ ! -d "/root/.ssh/" ]; then
+    mkdir -p /root/.ssh/ && chmod 700 /root/.ssh/
+fi
+
+# prepare ansible ssh keys
+if [ -f "/data/ssh/id_ansible_rsa" ]; then
+    cp /data/ssh/id_ansible_rsa /root/.ssh/id_rsa \
+    && chmod 600 /root/.ssh/id_rsa \
+    && echo "HostKey /root/.ssh/id_rsa" >> /etc/ssh/sshd_config
+fi
+
+if [ -f "/data/ssh/id_ansible_rsa.pub" ]; then
+    cp /data/ssh/id_ansible_rsa.pub /root/.ssh/id_rsa.pub
+fi
+
+# prepare authorized_keys
+if [ -f "/data/ssh/id_local_rsa.pub" ]; then
+    cp /data/ssh/id_local_rsa.pub /root/.ssh/authorized_keys
+fi
+
+# prepare known_hosts
+if [ -f "/data/ssh/known_hosts" ]; then
+    cp /data/ssh/known_hosts /root/.ssh/known_hosts
+fi
+
+# prepare ansible
+if [ -d "/data/ansible" ]; then
+    cp -r /data/ansible /etc/
+fi
+
+# export PATH
+# export PATH=/usr/src/app/env/bin/:$PATH
+
+exec "$@"
